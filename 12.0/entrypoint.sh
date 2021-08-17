73168093e6b08292047d7a1c95a21a3f2210cbac
Igor Visi <mail@igorvisi.com>
2021-08-17 13:34:42 +0100

    [IMP] Odoo 12.0-14.0: add debugpy & inotify
    In order to make a dev environment with debug and auto reload

--- 12.0/entrypoint.sh
+++ 12.0/entrypoint.sh
@@ -17,7 +17,7 @@ DB_ARGS=()
 function check_config() {
     param="$1"
     value="$2"
-    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then       
+    if grep -q -E "^\s*\b${param}\b\s*=" "$ODOO_RC" ; then
         value=$(grep -E "^\s*\b${param}\b\s*=" "$ODOO_RC" |cut -d " " -f3|sed 's/["\n\r]//g')
     fi;
     DB_ARGS+=("--${param}")
@@ -32,15 +32,15 @@ case "$1" in
     -- | odoo)
         shift
         if [[ "$1" == "scaffold" ]] ; then
-            exec odoo "$@"
+            exec python3 -m debugpy --listen 0.0.0.0:8888 odoo "$@"
         else
             wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-            exec odoo "$@" "${DB_ARGS[@]}"
+            exec python3 -m debugpy --listen 0.0.0.0:8888 odoo --dev=all "$@" "${DB_ARGS[@]}"
         fi
         ;;
     -*)
         wait-for-psql.py ${DB_ARGS[@]} --timeout=30
-        exec odoo "$@" "${DB_ARGS[@]}"
+        exec python3 -m debugpy --listen 0.0.0.0:8888 odoo "$@" "${DB_ARGS[@]}"
         ;;
     *)
         exec "$@"
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 About this Repo
 ======
 
-This is the Git repo of the official Docker image for [Odoo](https://registry.hub.docker.com/_/odoo/). See the Hub page for the full readme on how to use the Docker image and for information regarding contributing and issues.
-
-The full readme is generated over in [docker-library/docs](https://github.com/docker-library/docs), specifically in [docker-library/docs/odoo](https://github.com/docker-library/docs/tree/master/odoo).
+The purpose of this Git repo is fork of the official Docker image for [Odoo](https://registry.hub.docker.com/_/odoo).
+This image contains the packages debugpy ( An implementation of the Debug Adapter Protocol for Python ) and inotify ( For auto reload ).
